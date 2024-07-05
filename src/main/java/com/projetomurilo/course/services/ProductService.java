02e82f2f43ba1a9401f713398f81a08406184af7
Murilo <gamermurilo0@gmail.com>
2024-07-05 12:10:45 -0300

    Product Entity

---
+++ src/main/java/com/projetomurilo/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.projetomurilo.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetomurilo.course.entities.Product;
+import com.projetomurilo.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> opt = repository.findById(id);
+		return opt.get();
+	}
+}
